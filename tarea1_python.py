63dac6b776754df3e39f7b3e3dfe4e6c4c44152e
eric_d <eric_d_c_escutia@outlook.com>
2024-07-17 19:34:11 -0600

    Formas de comentar en python

---
+++ tarea1_python.py
@@ -0,0 +1,13 @@
+#https://www.python.org/
+
+#Podemos hacer comentarios de una línea utilizando el símbolo #
+
+#Aunque también podemos comentar
+#varias líneas utilizando múltiples
+#símbolor #, uno por cada línea
+
+"""
+Otra forma que tenemos para hacer comentarios
+de varias líneas es utilizando tres dobles 
+comillas; 3 al inicio y otras 3 al final 
+"""
