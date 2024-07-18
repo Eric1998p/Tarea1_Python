7b0a189d22bb53ac3181a73d2ddfbbf5362a6ca1
eric_d <eric_d_c_escutia@outlook.com>
2024-07-17 20:20:11 -0600

    Después de instalar el entorno virtual de python

--- tarea1_python.py
+++ tarea1_python.py
@@ -4,10 +4,43 @@
 
 #Aunque también podemos comentar
 #varias líneas utilizando múltiples
-#símbolor #, uno por cada línea
+#símbolos #, uno al inicio de cada línea
 
 """
 Otra forma que tenemos para hacer comentarios
 de varias líneas es utilizando tres dobles 
 comillas; 3 al inicio y otras 3 al final 
 """
+
+"""
+En Python, la distinción entre variables y constantes no es tan 
+estricta como en algunos otros lenguajes de programación. Python 
+no tiene palabras clave especiales para declarar constantes, pero 
+se pueden seguir ciertas convenciones para indicar que una variable 
+debe tratarse como una constante.
+
+En Python, la distinción entre variables y constantes no es tan 
+estricta como en algunos otros lenguajes de programación. Python no 
+tiene palabras clave especiales para declarar constantes, pero se 
+pueden seguir ciertas convenciones para indicar que una variable debe 
+tratarse como una constante. Aquí tienes una explicación detallada
+
+Para las constantes, la convención es escribir sus nombres en mayúsculas, 
+para indicar que su valor no debería cambiar.
+
+Las variables en Python se pueden declarar simplemente asignando un valor 
+a un nombre de variable. No se necesita una palabra clave especial para 
+declarar una variable.
+
+"""
+
+#Ejemplo de variable
+
+coche = "eléctrico"
+
+#Ejemplo de constante
+
+#Importaré una biblioteca para mandar a llamar a la constante pi
+import numpy as np
+
+PI = np.pi
